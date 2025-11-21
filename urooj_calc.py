54b3b85ad187548cd4a5a0c0ebeee96414183f05
Urooj-Fatima-I <uroojiftikhar718@gmail.com>
2025-11-21 09:51:30 +0500

    I added python calculator

---
+++ urooj_calc.py
@@ -0,0 +1,25 @@
+#making function for operations
+def urooj_calc():
+    
+    while True:
+        print("\nSelect Option:")
+        print("1. Calculations")
+        print("2. Exit")
+        choice = input("Enter choice: ")
+
+        if choice == "2":
+            print("Exiting")
+            break
+
+        elif choice =="1":
+            a = float(input("\nEnter first number : "))
+            b = float(input("Enter second number: "))
+            print ("\nAddition:    ", a + b)
+            print ("Subtraction:  ",round( a - b))
+            print ("Product:   ", a * b)
+            print ("Division:   ", round(a / b,2) if b != 0 else "Division by zero is not possible try another number")
+ 
+        else:
+            print("Invalid choice")
+#calling function
+urooj_calc()
